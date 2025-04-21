ebca7031b28635f8d250f03b8bde9f034b987e9c
izzyjosh <calebayuba222@gmail.com>
2025-04-21 01:47:54 +0100

    fix: deployment fixtures

--- chatgpt/vite.config.js
+++ chatgpt/vite.config.js
@@ -4,6 +4,5 @@ var vite_1 = require("vite");
 var plugin_react_1 = require("@vitejs/plugin-react");
 // https://vite.dev/config/
 exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_1.default)()],
-    base: process.env.VITE_BASE_PATH || "/ChatGPT_Clone/chatgpt"
+    plugins: [(0, plugin_react_1.default)()]
 });
